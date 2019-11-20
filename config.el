5b4f15d9278aa436a0d8d08e1fedaeaa37b0b2e4
Pierre-antoine Comby <comby@crans.org>
2019-11-20 10:17:10 +0100

    add language check support

--- config.el
+++ config.el
@@ -71,6 +71,21 @@
    :nv "DEL" #'dired-up-directory)
   )
 
+(setq  ispell-dictionary "francais" )
+
+(let ((langs '("english" "francais" )))
+  (setq lang-ring (make-ring (length langs)))
+  (dolist (elem langs) (ring-insert lang-ring elem)))
+
+(defun cycle-ispell-languages ()
+  (interactive)
+  (let ((lang (ring-ref lang-ring -1)))
+    (ring-insert lang-ring lang)
+    (ispell-change-dictionary lang)))
+
+(global-set-key [f6] 'cycle-ispell-languages)
+
+
 ;; -----------------
 ;;  LaTeX
 ;; -----------------
