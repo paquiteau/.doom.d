261a6447d794753b7005ca84e46b0b57602866dc
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2021-11-19 14:06:28 +0100

    fix font ligatures issues

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+systemctl --user enable emacs.service
+
 cd /tmp
 curl -o "aspell6-en-custom.tar.bz2" 'http://app.aspell.net/create?max_size=80&spelling=GBs&spelling=US&max_variant=0&diacritic=keep&special=hacker&special=roman-numerals&encoding=utf-8&format=inline&download=aspell'
 tar -xjf "aspell6-en-custom.tar.bz2"
