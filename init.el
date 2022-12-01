8207d5cfb8693644b979683030f187531419dcde
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-12-01 14:28:52 +0100

    house keeping

--- init.el
+++ init.el
@@ -45,7 +45,8 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       (treemacs +lsp)
+       (treemacs          ; a project drawer, like neotree but cooler
+        +lsp)
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter         ; vcs diff in the fringe
         +diff-hl
@@ -106,6 +107,7 @@
        (lookup              ; navigate your code and its documentation
         +dictionnary
         +docsets)
+       tree-sitter
 
        (lsp               ; M-x vscode
         +peek)
@@ -161,6 +163,7 @@
        (latex             ; writing papers in Emacs has never been so fun
         +latexmk
         +cdlatex
+   ;;     +lsp
         +fold)
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
@@ -184,6 +187,7 @@
        ;;purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
         +lsp
+        +tree-sitter
         +pyenv
         +cython
         )
@@ -191,7 +195,7 @@
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
-       ;;rst               ; ReST in peace
+       rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
